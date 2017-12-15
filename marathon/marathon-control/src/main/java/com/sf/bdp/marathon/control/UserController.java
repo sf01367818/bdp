70c6f538dace7555144eeaf764b70a8c8d5e310e
chenjun10@sf-express.com <chenjun10@sf-express.com>
2017-12-15 16:46:34 +0800

    修改GroupUser相关操作类

--- marathon/marathon-control/src/main/java/com/sf/bdp/marathon/control/UserController.java
+++ marathon/marathon-control/src/main/java/com/sf/bdp/marathon/control/UserController.java
@@ -1,29 +1,26 @@
 package com.sf.bdp.marathon.control;
 
+import com.sf.bdp.marathon.common.bean.Response;
+import com.sf.bdp.marathon.service.UserService;
 import javax.annotation.Resource;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import com.sf.bdp.marathon.common.bean.Response;
-import com.sf.bdp.marathon.entity.User;
-import com.sf.bdp.marathon.service.UserService;
 
 @Controller
 @RequestMapping(value = "user", produces = "application/json; charset=UTF-8")
 public class UserController {
 
-	@Resource
-	private UserService userService;
-	
-	@RequestMapping("get")
-	@ResponseBody
-	public Response get(String id) {
-		try {
-			return Response.ok(userService.get(id));
-		} catch (Exception e) {
-			return Response.error(e.getMessage());
-		}
-	}
+  @Resource
+  private UserService userService;
+
+  @RequestMapping("get")
+  @ResponseBody
+  public Response get(String id) {
+    try {
+      return Response.ok(userService.get(id));
+    } catch (Exception e) {
+      return Response.error(e.getMessage());
+    }
+  }
 }
