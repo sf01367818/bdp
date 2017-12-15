246b9190c5c04207fccc99c131c08d358218b2c6
chenjun10@sf-express.com <chenjun10@sf-express.com>
2017-12-15 16:54:20 +0800

    修改sonarLint坏味道

--- marathon/marathon-control/src/main/java/com/sf/bdp/marathon/control/UserController.java
+++ marathon/marathon-control/src/main/java/com/sf/bdp/marathon/control/UserController.java
@@ -3,6 +3,7 @@ package com.sf.bdp.marathon.control;
 import com.sf.bdp.marathon.common.bean.Response;
 import com.sf.bdp.marathon.service.UserService;
 import javax.annotation.Resource;
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -10,6 +11,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @RequestMapping(value = "user", produces = "application/json; charset=UTF-8")
 public class UserController {
+
+  private static final Logger logger = Logger.getLogger(UserController.class);
 
   @Resource
   private UserService userService;
@@ -20,6 +23,7 @@ public class UserController {
     try {
       return Response.ok(userService.get(id));
     } catch (Exception e) {
+      logger.error(e.getMessage(), e);
       return Response.error(e.getMessage());
     }
   }
