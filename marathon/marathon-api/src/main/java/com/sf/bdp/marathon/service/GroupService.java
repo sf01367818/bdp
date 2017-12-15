744d5f406a576ebca3e94862df267fb32c4f9209
sf01367818 <34528923+sf01367818@users.noreply.github.com>
2017-12-15 13:02:55 +0800

    接口
    Group和Market接口

--- marathon/marathon-api/src/main/java/com/sf/bdp/marathon/service/GroupService.java
+++ marathon/marathon-api/src/main/java/com/sf/bdp/marathon/service/GroupService.java
@@ -1,21 +1,29 @@
 package com.sf.bdp.marathon.service;
 
-import com.sf.bdp.marathon.common.bean.Response;
 import com.sf.bdp.marathon.entity.Group;
 
 /**
- * 集货团用户Service
+ * 集货团Service
  *
  * @author
  */
 public interface GroupService {
 
-  /**
-   * 生成集货团用户关系
-   *
-   * @param group
-   * @return
-   */
-  Response gernateGroup(Group group);
+    /**
+     * create a new group
+     *
+     * @param groupId
+     * @return
+     */
+    Group getGroup(String groupId);
+
+
+    /**
+     * create a new group
+     *
+     * @param mktId
+     * @return
+     */
+    Group getCurrentGroup(String mktId);
 
 }
