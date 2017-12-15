97e81b17f811e82401a4b56b300f0be545e54cf4
chenjun10@sf-express.com <chenjun10@sf-express.com>
2017-12-15 16:57:02 +0800

    修改实体类的坏味道

--- marathon/marathon-api/src/main/java/com/sf/bdp/marathon/entity/MarketBase.java
+++ marathon/marathon-api/src/main/java/com/sf/bdp/marathon/entity/MarketBase.java
@@ -1,13 +1,11 @@
 package com.sf.bdp.marathon.entity;
 
 import java.io.Serializable;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
-
 import org.hibernate.annotations.GenericGenerator;
 
 @Entity
@@ -17,27 +15,27 @@ public class MarketBase implements Serializable {
 	private static final long serialVersionUID = 1337718021661802250L;
 	
 	@Id
-	@Column(name="mkt_id",columnDefinition = ("varchar(36) not null comment '专业市场ID'"))
+	@Column(name = "mkt_id", columnDefinition = "varchar(36) not null comment '专业市场ID'")
 	@GeneratedValue(generator = "uuid") 
 	@GenericGenerator(name = "uuid", strategy = "uuid")
 	private String mktId;
-	@Column(name="mkt_name_show",columnDefinition = ("varchar(50) not null comment '市场外部名称'"))
+	@Column(name = "mkt_name_show", columnDefinition = "varchar(50) not null comment '市场外部名称'")
 	private String mktNameShow;
-	@Column(name="daily_min_packages",columnDefinition = ("smallint comment '单客日均最小件量'"))
-	private Short dailyMinPackages;	
-	@Column(name="weight_min",columnDefinition = ("decimal comment '重量区间（最小）'"))
+	@Column(name = "daily_min_packages", columnDefinition = "smallint comment '单客日均最小件量'")
+	private Short dailyMinPackages;
+	@Column(name = "weight_min", columnDefinition = "decimal comment '重量区间（最小）'")
 	private Float weightMin;
-	@Column(name="weight_max",columnDefinition = ("decimal comment '首重价格'"))
+	@Column(name = "weight_max", columnDefinition = "decimal comment '首重价格'")
 	private Float weightMax;
 	@Column(name="base_price",columnDefinition = ("decimal comment '首重价格'"))
 	private Float basePrice;
-	@Column(name="base_weight",columnDefinition = ("decimal comment '首重重量'"))
+	@Column(name = "base_weight", columnDefinition = "decimal comment '首重重量'")
 	private Float baseWeight;
-	@Column(name="group_limit",columnDefinition = ("smallint comment '成团人数'"))
+	@Column(name = "group_limit", columnDefinition = "smallint comment '成团人数'")
 	private Short groupLimit;
-	@Column(name="group_duration",columnDefinition = ("tinyint comment '拼团周期'"))
+	@Column(name = "group_duration", columnDefinition = "tinyint comment '拼团周期'")
 	private Short groupDuration;
-	@Column(name="use_require",columnDefinition = ("text comment '使用要求'"))
+	@Column(name = "use_require", columnDefinition = "text comment '使用要求'")
 	private String useRequire;//	使用要求
 	public String getMktId() {
 		return mktId;
